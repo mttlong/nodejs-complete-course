e035e570d622df7b005acf4e11d5bb8236b6032f
Mark Long <mark.long@asiamobiliti.com>
2019-04-13 23:02:06 +0800

    Added the 404 controller. Also fixed a bug in the navigation header for 404.ejs.

--- lecture-modules/sect7/sect7-92/app.js
+++ lecture-modules/sect7/sect7-92/app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const errController = require('./controllers/error');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -11,14 +13,13 @@ app.set('views', 'views');
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page Not Found' });
-});
+app.use(errController.get404);
 
 app.listen(3000);
